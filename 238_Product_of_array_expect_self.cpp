bf365dc83d5cd9a16f09b7f3134e989fc06422e9
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-10-24 06:29:49 +0000

    238

--- 238_Product_of_array_expect_self.cpp
+++ 238_Product_of_array_expect_self.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans(nums.size(),1); 
+        vector<int> ans(nums.size(),1); //o(n^2)time
         for (int i=0;i<nums.size();i++){
             for (int j=0;j<nums.size();j++){
                 if(i!=j){

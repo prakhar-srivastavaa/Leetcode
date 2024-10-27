215178a615fbc4e7a3028e02c0eacbb267bdb64a
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-10-27 07:44:18 +0000

    Binarry search but getting TLE

--- 852_Peak_Index_in_a_Mountain_Array.cpp
+++ 852_Peak_Index_in_a_Mountain_Array.cpp
@@ -1,14 +1,18 @@
-// sloving through linear search in time of O(n)
+// through Binary search but getting TLE.
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n = arr.size();
-        int peak = 0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>arr[peak]){
-                peak=i;
+        int start=0;
+        int end=n-1;
+        int mid= start+(end-start)/2;
+        int peak=0;
+        while (true){
+            // if ()
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
             }
         }
-        return peak;
+
     }
 };

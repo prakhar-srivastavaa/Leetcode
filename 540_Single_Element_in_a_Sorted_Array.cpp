c51d11617f1189777384a2d8869fe92f6b513c60
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-10-28 15:40:07 +0000

    best Approach

--- 540_Single_Element_in_a_Sorted_Array.cpp
+++ 540_Single_Element_in_a_Sorted_Array.cpp
@@ -8,6 +8,7 @@ public:
         
         while(start<=end){
             int mid = start+(end-start)/2;
+            if (mid==0&&nums[0]!= nums[1]) return nums[mid];
             if (nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }

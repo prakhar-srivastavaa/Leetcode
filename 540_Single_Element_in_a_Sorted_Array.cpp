22ca9ae0feb9e1fb0568bbdf5cb60b72f9f91365
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-10-28 15:39:45 +0000

    TLE

---
+++ 540_Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        if (n==1) return nums[0];
+        
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if (nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            else if (mid%2==0){//even
+                if(nums[mid-1]==nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+            else{//odd
+                if(nums[mid]==nums[mid-1]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};

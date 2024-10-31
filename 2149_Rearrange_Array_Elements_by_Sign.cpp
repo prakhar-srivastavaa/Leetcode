ebbad717db2777ab88b34fa6e204bfb4b0c16e15
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-10-31 11:22:25 +0000

    Optimal

--- 2149_Rearrange_Array_Elements_by_Sign.cpp
+++ 2149_Rearrange_Array_Elements_by_Sign.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        // int n = nums.size();
         vector<int> ans(nums.size(),0);
         int Positive=0; int negative=1;
         for (int i=0;i<nums.size();i++){
             if (nums[i]<0){//negative no.
                 ans[negative]=nums[i];
                 negative+=2;
-            }else{
+            }else{//postive
                 ans[Positive]=nums[i];
                 Positive+=2;
             }

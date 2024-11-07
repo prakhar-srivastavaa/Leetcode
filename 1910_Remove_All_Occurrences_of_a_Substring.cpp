a147eaa3c52e8a4f4073eb3fd734abf6314a1747
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-11-07 14:14:29 +0000

    optimal

--- 1910_Remove_All_Occurrences_of_a_Substring.cpp
+++ 1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public:// find(part) will return 1st occouarnce index of part
     string removeOccurrences(string s, string part) {
         while(s.length() > 0 && s.find(part) < s.length()){
             s.erase(s.find(part), part.length());

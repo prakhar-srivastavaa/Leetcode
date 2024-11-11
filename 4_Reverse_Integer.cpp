dc3d9b186c943cde891435aed674e0047a81500e
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-11-11 17:51:46 +0000

    optimal

--- 4_Reverse_Integer.cpp
+++ 4_Reverse_Integer.cpp
@@ -4,6 +4,9 @@ public:
         int reNum=0;
         while(x!=0){
             int digit =x%10;
+            if(reNum>INT_MAX/10|| reNum<INT_MIN/10){
+                return 0;
+            }
             reNum= reNum*10+digit;
             x=x/10;
         }

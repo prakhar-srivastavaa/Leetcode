450e80f60fbec7934eaf600a1b9c19e097e69dc3
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-11-12 10:27:25 +0000

    optimal

--- 9_Palindrome_number.cpp
+++ 9_Palindrome_number.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int reverse(int x){
-        int revN=0;
-        while (x!=0){
-            int digit=x%10;
-            if(revN>INT_MAX/10||revN<INT_MIN/10){
-                return 0;
-            }
-            revN=revN*10+digit;
-…        }else {
-            return 0;
-        }   
+    bool isPalindrome(int x) {
+        long rev=0;
+        int temp=x;
+        while(x>0){
+            rev=rev*10+x%10;
+            x=x/10;
+        }
+        return (temp==rev);
     }
 };

57db14063ddc7d8fddf22618bb34f6d33c2df39c
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2024-11-14 06:44:09 +0000

    oops used for optimal performance

--- 74_Search_a_2D_Matrix.cpp
+++ 74_Search_a_2D_Matrix.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int startRow = 0, endRow = m - 1;
+    bool searchinRow(vector<vector<int>>& matrix, int target, int row, int n){
+        int st=0,end=n-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if (target==matrix[row][mid]){
+                return true;
+            }else if(target > matrix[row][mid]){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return false;
 
-        while (startRow <= endRow) {
-            int midRow = startRow + (endRow - startRow) / 2;
-            if (target >= matrix[midRow][0] && target <= matrix[midRow][n - 1]) {
-                int startCol = 0, endCol = n - 1;
-                while (startCol <= endCol) {
-                    int midCol = startCol + (endCol - startCol) / 2;
-                    if (matrix[midRow][midCol] == target) {
-                        return true;
-                    } else if (matrix[midRow][midCol] < target) {
-                        startCol = midCol + 1;
-                    } else {
-                        endCol = midCol - 1;
-                    }
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // //bs on totao no of rows 
+        int m = matrix.size();
+        int n= matrix[0].size();
+        int startRow=0,endRow=m-1;
+        while(startRow <= endRow){
+            int midRow= startRow +(endRow-startRow)/2;
+            if(target>=matrix[midRow][0]&& target<=matrix[midRow][n-1]){
+                //found row now bS on row
+                return searchinRow(matrix,target,midRow,n);
+            }else if(target >= matrix[midRow][n-1]){
+                //d0wn => right
+                startRow=midRow+1;
+            }else{
+                //up=> left
+                endRow=midRow-1;
+            }
         }
         return false;
     }

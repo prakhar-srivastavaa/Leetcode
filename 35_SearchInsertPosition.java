4152da1cb6342bf52078c88c125ba2c9e3686673
Prakhar Srivastava <prakharbksc9@gmail.com>
2025-07-02 10:37:49 +0530

    Binary Search O(logn)

--- 35_SearchInsertPosition.java
+++ 35_SearchInsertPosition.java
@@ -1,17 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            arr.add(nums[i]);
-        }
-        arr.add(target);
-        Collections.sort(arr);
-        
-        for(int i=0;i<arr.size();i++){
-            if(arr.get(i)==target){
-               return i;
+        int left =0, right = nums.length-1;
+        while(left<=right){
+            int mid =left+ (right-left)/2;
+            if(nums[mid] < target){
+                left=mid+1;
+            }
+            else if(nums[mid]==target){
+                return mid;
+            }else{
+                right=mid-1;
             }
         }
-        return 0;
+        return left;
     }
 }

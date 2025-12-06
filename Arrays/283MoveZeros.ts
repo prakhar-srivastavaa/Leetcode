89eac3cda18f6559ec7b4a15b29d6a55ec7435e8
prakhar-srivastavaa <prakharbksc9@gmail.com>
2025-12-06 18:44:49 +0530

    283Moves-zeros

---
+++ Arrays/283MoveZeros.ts
@@ -0,0 +1,13 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    let countnonzero=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            // swaping sing index
+            [nums[i],nums[countnonzero]]=[nums[countnonzero],nums[i]];
+            countnonzero++;
+        }
+    }  
+};

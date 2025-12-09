790a5946d712f538fb2376b92a98e254554bd3a5
prakhar-srivastavaa <prakharbksc9@gmail.com>
2025-12-09 18:53:40 +0530

    392

---
+++ Arrays/String/392IsSubsequence.ts
@@ -0,0 +1,10 @@
+function isSubsequence(s: string, t: string): boolean {
+    let i=0 , j=0;
+    while(i<s.length&&j<t.length){
+        if(s[i]==t[j]){
+            i++;
+        }
+        j++;
+    }
+    return i===s.length
+};

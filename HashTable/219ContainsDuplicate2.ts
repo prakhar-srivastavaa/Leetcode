8d72abf0161c189f387d5d20b3f36e482500f66b
prakhar-srivastavaa <prakharbksc9@gmail.com>
2025-12-23 03:06:51 +0530

    219

--- HashTable/219ContainsDuplicate2.ts
+++ HashTable/219ContainsDuplicate2.ts
@@ -1,4 +1,10 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-
-    
+        const map = new Map<number, number>();
+        for (let i=0 ;i<nums.length;i++){
+            if(map.has(nums[i])&& i-(map.get(nums[i]))<=k){//3-1//2-1
+                return true;
+            }
+            map.set(nums[i],i);//10,21,32,13//10,01,
+        }
+        return false;
 };

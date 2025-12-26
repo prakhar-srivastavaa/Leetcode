afcdbd4d317fc0b7b4ef39e5a1f63725dc6f41dc
prakhar-srivastavaa <prakharbksc9@gmail.com>
2025-12-26 14:17:37 +0530

    best-two pointer

--- TwoPointers/167TwoSumII-InputArrayIsSorted.ts
+++ TwoPointers/167TwoSumII-InputArrayIsSorted.ts
@@ -1,9 +1,15 @@
 function twoSum(numbers: number[], target: number): number[] {
-    for(let i=0;i<numbers.length;i++){
-        for (let j=i+1; j<numbers.length;j++){
-            if (numbers[i] + numbers[j]== target){
-                return [i+1,j+1];
-            }
+    let l=0;
+    let r=numbers.length-1;
+    while(l<r){
+        if(numbers[l]+numbers[r]==target){
+            return [l+1,r+1];
+        }
+        if(numbers[l]+numbers[r]<target){
+            l++;
+        }
+        else{
+            r--;
         }
     }
 };

82a7dd5b047fd846216534199750d12a962e3517
prakhar-srivastavaa <prakharbksc9@gmail.com>
2025-12-28 23:31:14 +0530

    3sum

--- TwoPointers/15-3Sum.ts
+++ TwoPointers/15-3Sum.ts
@@ -1,27 +1,25 @@
 function threeSum(nums: number[]): number[][] {
-  const result: number[][] = [];
-  nums.sort((a, b) => a - b); // Sort the array first
-
-  for (let i = 0; i < nums.length - 2; i++) {
-    // To avoid duplicates
-    if (i === 0 || nums[i] !== nums[i - 1]) {
-      let left = i + 1, right = nums.length - 1;
-      while (left < right) {
-        const sum = nums[i] + nums[left] + nums[right];
-        if (sum === 0) {
-          result.push([nums[i], nums[left], nums[right]]);
-          // Skip duplicates for `left` and `right`
-          while (left < right && nums[left] === nums[left + 1]) left++;
-          while (left < right && nums[right] === nums[right - 1]) right--;
-          left++;
-          right--;
-        } else if (sum < 0) {
-          left++;
-        } else {
-          right--;
+    const result = [];
+    nums=nums.sort((a,b)=>(a-b));
+    for(let i=0;i<nums.length;i++){
+        if(i==0 || nums[i]!=nums[i-1]){
+            let l=i+1;
+            let r=nums.length-1;
+            while(l<r){
+                let threeSum= nums[i]+nums[l]+nums[r];
+                if (threeSum>0){
+                    r--;
+                }
+                else if(threeSum<0){
+                    l++;
+                }
+                else{
+                    result.push([nums[i],nums[l],nums[r]])
+                    l++;
+                    r--;
+                }
+            }
         }
-      }
     }
-  }
-  return result;
+    return result;
 }

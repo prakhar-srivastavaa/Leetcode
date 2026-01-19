e7a6ab336bb02c6158cd461ebd1c661f34f69e45
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-01-19 21:30:21 +0530

    Rotate by 90degree image

---
+++ Matrix(2D-Array)/RotateImage.ts
@@ -0,0 +1,20 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    let n = matrix.length;
+    //transposing
+    for (let i=0;i<n-1 ;i++){
+        for (let j=i+1;j<n;j++){
+            // const temp= matrix[i][j];
+            // matrix[i][j]=matrix[j][i];
+            // matrix[j][i]= temp;
+            [matrix[i][j], matrix[j][i]]=[matrix[j][i],matrix[i][j]];
+        }
+    }
+    // reverssing now
+    for(let i=0;i<n;i++){
+        matrix[i].reverse();
+    }
+    
+};

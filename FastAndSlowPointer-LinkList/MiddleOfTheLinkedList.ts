c85a877a6d0f3cef84f7e2fb5d7fdbaba84c7557
Prakhar Srivastava <prakharbksc9@gmail.com>
2026-02-01 20:16:28 +0530

    fast and slow pointer

--- FastAndSlowPointer-LinkList/MiddleOfTheLinkedList.ts
+++ FastAndSlowPointer-LinkList/MiddleOfTheLinkedList.ts
@@ -1,6 +1,6 @@
 function middleNode(head: ListNode | null): ListNode | null {
     let slow: ListNode=head;
-    let fast:ListNode= head;
+    let fast:ListNode|null= head;
     while(fast!==null && fast.next!==null){
         slow=slow.next;
         fast=fast.next.next;
